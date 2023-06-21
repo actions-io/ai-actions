94331322acafbcde04bc7291e728a76edc7f7a47
Anatoly Stansler <29913247+anatolystansler@users.noreply.github.com>
2023-06-21 20:35:38 +0300

    add OpenAI function

--- src/controllers/monday-controller.js
+++ src/controllers/monday-controller.js
@@ -2,38 +2,56 @@ const mondayService = require('../services/monday-service');
 const transformationService = require('../services/transformation-service');
 const { TRANSFORMATION_TYPES } = require('../constants/transformation');
 
+// Route: Action text transformation.
 async function executeAction(req, res) {
   const { shortLivedToken } = req.session;
   const { payload } = req.body;
 
   try {
     const { inputFields } = payload;
-    const { boardId, itemId, sourceColumnId, targetColumnId, transformationType } = inputFields;
+    const {
+      boardId,
+      itemId,
+      sourceColumnId,
+      targetColumnId,
+      transformationType
+    } = inputFields;
 
+    console.log("[DEBUG] Action inputs:", inputFields);
+
+    // Get current value from source column.
     const text = await mondayService.getColumnValue(shortLivedToken, itemId, sourceColumnId);
     if (!text) {
       return res.status(200).send({});
     }
+
+    // Apply text transformations.
     const transformedText = transformationService.transformText(
       text,
       transformationType ? transformationType.value : 'TO_UPPER_CASE'
     );
 
+    // Set new value for target column.
     await mondayService.changeColumnValue(shortLivedToken, boardId, itemId, targetColumnId, transformedText);
 
     return res.status(200).send({});
   } catch (err) {
     console.error(err);
-    return res.status(500).send({ message: 'internal server error' });
+    return res.status(500).send({ message: 'Internal Server Error' });
   }
 }
 
+// TODO(Anatoly): Route image generation and upload.
+// TODO(Anatoly): Route AI Assistant text transformation.
+
+// Route: Get available transformation types.
+// TODO(Anatoly): Change to prompt templates.
 async function getRemoteListOptions(req, res) {
   try {
     return res.status(200).send(TRANSFORMATION_TYPES);
   } catch (err) {
     console.error(err);
-    return res.status(500).send({ message: 'internal server error' });
+    return res.status(500).send({ message: 'Internal Server Error' });
   }
 }
 
--- src/services/openai-service.ts
+++ src/services/openai-service.ts
@@ -1,26 +1,33 @@
-// TODO(Anatoly): Use GPT3.5.
-
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  
+
 const openai = new OpenAIApi(configuration);
 
-// TODO: handle errors from openAI
-export class OpenAiService {
-    async getCompletionsFromOpenAi(prompt:string, n: number) {
-    try {
-    const completionsFromApi = await openai
-        .createCompletion({
-          model: "text-davinci-003",
-          prompt,
-          n,
-        })
-      return completionsFromApi.data.choices;
-    } catch (err) {
-      console.log(err);
-    }
+const getCompletion = async (prompt:string) => {
+  try {
+    const completions = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are friendly assistant working in automated workflows environment.",
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+
+    return completions.data.choices[0].message.content;
+  } catch (err) {
+    console.log(err);
   }
+}
+
+module exports = {
+  getCompletion,
 }
